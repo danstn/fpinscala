1e603081bb670195681fbc623715fe7e80e5644f
Daniel Stankevich <daniel.stankevich@rea-group.com>
2015-05-08 09:11:20 +1000

    dropWhile recursive

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -67,7 +67,10 @@ object List { // `List` companion object. Contains functions for creating and wo
     }
   }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile(t, f)
+    case _ => Nil
+  }
 
   def init[A](l: List[A]): List[A] = sys.error("todo")
 
