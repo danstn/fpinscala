167e06fe63c3d6828ecfd54d881d544b80a365c8
Daniel Stankevich <daniel.stankevich@rea-group.com>
2015-05-11 21:31:16 +1000

    dropWhile should actually return something

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -69,7 +69,7 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Cons(h, t) if f(h) => dropWhile(t, f)
-    case _ => Nil
+    case _ => l
   }
 
   def init[A](l: List[A]): List[A] = l match {
