7124b945e5bf258c65c3761020c3a2d85bb7a2f8
Daniel Stankevich <daniel.stankevich@rea-group.com>
2015-05-25 23:02:41 +1000

    Append using foldRight

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -31,11 +31,13 @@ object List { // `List` companion object. Contains functions for creating and wo
     case _ => 101
   }
 
-  def append[A](a1: List[A], a2: List[A]): List[A] =
-    a1 match {
+  def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
       case Nil => a2
       case Cons(h,t) => Cons(h, append(t, a2))
     }
+
+  def append2[A](a1: List[A], a2: List[A]): List[A] = 
+    foldRight(a1, a2)(Cons(_, _))
 
   def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     as match {
